69b5ec14ae6464967e054e91661d4568d56bbba1
Vishal Anand <anandvishal1302@gmail.com>
2026-03-24 00:08:54 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Typing Effect
+const text = ["Full Stack Developer", "MERN Developer"];
+let i = 0, j = 0, current = "", del = false;
+
+function type() {
+  if (!del) current = text[i].substring(0, j++);
+  else current = text[i].substring(0, j--);
+
+  document.getElementById("typing").innerHTML = current;
+
+  if (j === text[i].length) del = true;
+  if (j === 0) {
+    del = false;
+    i = (i + 1) % text.length;
+  }
+
+  setTimeout(type, del ? 150 : 150);
+}
+type();
+
+// Scroll Progress
+window.onscroll = () => {
+  let sc = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+  document.getElementById("progress").style.width = sc + "%";
+};
